8ed52aac3aedb263de672faada0a6a2dc34f6686
Arpan Das <dasarpan471@gmail.com>
2022-08-06 11:36:14 +0530

    complex number have been learned

---
+++ Complex/index.jl
@@ -0,0 +1,48 @@
+println("How to multiply two complex number")
+
+println((1 + 2im)*(2 - 3im))
+
+println("How to divide two complex number")
+
+println((1 + 2im)/(1 - 2im))
+
+println("How to add two complex number")
+
+println((1 + 2im) + (1 - 2im))
+
+println("how to subtract two complex number")
+
+println((1 + 2im) - (1 - 2im))
+
+println("How to square two complex number")
+
+println((-1 + 2im)^2)
+
+z = 1 + 2im
+
+println("To get real part of complex number")
+
+println(real(z))
+
+println("To get imaginary part of complex number")
+
+println(imag(z))
+
+println("To conjugate the complex number")
+
+println(conj(z))
+
+println("To get absolute value of complex number")
+
+println(abs(z))
+
+println("To get the squared absolute value of complex number")
+
+println(abs2(z))
+
+
+println("To get phase angles in radians")
+
+println(angle(z))
+
+
---
+++ Complex/rational.jl
@@ -0,0 +1,17 @@
+# Julia has a rational number type to represent exact ratios of integers. Rationals are constructed using the // operator:
+
+println(6//9)
+
+println("to show the numerator of rational number in julia")
+
+println(numerator(2//3))
+
+println("to show the denominator of rational number in julia")
+
+println(denominator(2//3))
+
+println(2//3 == 6//9)
+
+println(2//3 == 6//10)
+
+println( float(3//4))
